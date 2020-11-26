91a55e046f96be8784dc3a09781054b2d85341ff
parthurp <arthur.pate@yncrea.fr>
2020-11-26 19:06:20 +0100

    the work goes on...

--- utils_audio.py
+++ utils_audio.py
@@ -1,5 +1,7 @@
 import glob
 import os
+import pandas as pd
+import numpy as np
 
 from lsb import lsb_apply, lsb_read, getnframes, lsb_decode
 from utils import compareFiles, decodeKeyFile
@@ -23,6 +25,113 @@ def mp3_to_wav(inputdir, **kwargs):
     if rm_mp3:
         for filename in glob.glob(inputdir + '*.mp3'):
             os.system('rm ' + filename)
+
+###################################
+def selectSounds_FMA(input_dir, input_file, genre_dir, genre_catalog, **kwargs):
+
+    min_dur=kwargs.get('min_dur', 0)
+    max_dur=kwargs.get('max_dur', 600)
+    genre=kwargs.get('genre', 'Metal')
+    verbose=kwargs.get('verbose', 0)
+
+    # --- convert genre to numeric value
+    genres_list = pd.read_csv(genre_dir+genre_catalog)
+    genre_id = str(int(np.array(genres_list.where(genres_list['title']==genre).dropna(how='all').dropna(axis=1))))
+
+    # --- load metadata
+    tracks = utilsFMA.load(dir_metadata + 'tracks.csv')
+
+    tab_id_duration = tracks['track', 'duration']
+    track_ids = tab_id_duration.index
+    track_durations = tab_id_duration.values
+
+    tab_id_genres = np.array(tracks['track', 'genres'])
+
+    # --- loop over the metadata, select one genre en restrict to a given duration
+    number_of_entries = 0; number_of_matching_entries = 0
+    list_matching_keys = []
+    for index in range(len(tab_id_duration)):
+        if verbose:
+            pass
+        current_genre = tab_id_genres[index]
+        current_duration = track_durations[index]
+        for current_genre_item in current_genre:
+            if current_genre_item == genre_id:
+                if current_duration > min_dur and current_duration < max_dur:
+                    #print(key)
+                    number_of_matching_entries+=1
+                    list_matching_keys.append(tab_ids[index])
+        number_of_entries+=1
+
+    print('There were ' + str(number_of_entries) + ' entries in the data base')
+    print('There were ' + str(number_of_matching_entries) + ' matching entries in the data base')
+    #print('Matching keys (track_id) are :' + str(list_matching_keys))
+
+    return list_matching_keys,track_ids
+
+###################################
+def copySounds_FMA(list_keys_to_files,catalog,source_dir,destination_dir):
+    # --- Retrieve the corresponding files and store them locally
+    for numfile in range(len(list_keys_to_files)):
+        current_id = list_keys_to_files[numfile]
+        current_id_str = '{:06d}'.format(current_id)
+        current_path = os.path.join(source_dir, current_id_str[:3], current_id_str + '.mp3')
+
+        if os.path.exists(current_path):
+            print('Found file ' + str(current_path) + '! -> retrieving it...')
+            os.system('cp ' + current_path + ' ' + destination_dir + str(current_id) + '.mp3')
+        else:
+            print('File ' + str(current_path) + ' not found... trying next one')
+
+    #f = utilsFMA.get_audio_path(dir_downloaded_db, 55123) # /!\ --> change 2 with the key that is wanted
+    #filename = f.split('/')[-1]
+    #path = '/'.join(f.split('/')[:-1]) + '/'
+
+    return 0
+
+###################################
+def selectSounds_MTG(input_dir, input_file, **kwargs):
+
+    min_dur=kwargs.get('min_dur', 0)
+    max_dur=kwargs.get('max_dur', 600)
+    genre=kwargs.get('genre', 'metal')
+    verbose=kwargs.get('verbose', 0)
+
+    # --- load metadata
+    tracks, tags, extra = commons.read_file(input_dir + input_file)
+    # fields of 'tracks' are: artist_id, album_id, genre, instrument, mood/theme, path, dration, tags
+    #print(tracks)
+
+    # --- loop over the metadata, select one genre en restrict to a given duration
+    number_of_entries = 0; number_of_matching_entries = 0
+    list_matching_keys = []
+    for key in tracks:
+        if verbose:
+            print('-----------' + '\n current entry: \n')
+            print('artist_id:' + str(tracks[key]['artist_id']))
+            print('album_id:' + str(tracks[key]['album_id']))
+            print('genre:' + str(tracks[key]['genre']))
+            print('instrument:' + str(tracks[key]['instrument']))
+            print('mood/theme:' + str(tracks[key]['mood/theme']))
+            print('path:' + str(tracks[key]['path']))
+            print('duration:' + str(tracks[key]['duration']))
+            print('tags:' + str(tracks[key]['tags']))
+
+        current_genre = tracks[key]['genre']
+        current_duration = tracks[key]['duration']
+        for current_genre_item in current_genre:
+            if current_genre_item == genre:
+                if current_duration > min_dur and current_duration < max_dur:
+                    #print(key)
+                    number_of_matching_entries+=1
+                    list_matching_keys.append(key)
+        number_of_entries+=1
+
+    print('There were ' + str(number_of_entries) + ' entries in the data base')
+    print('There were ' + str(number_of_matching_entries) + ' matching entries in the data base')
+    #print('Matching keys (track_id) are :' + str(list_matching_keys))
+
+    return list_matching_keys, tracks
 
 ###################################
 def copySounds_MTG(list_keys_to_files,catalog,source_dir,destination_dir):
--- work-with-FMA-db.py
+++ work-with-FMA-db.py
@@ -3,41 +3,66 @@
 import audioread
 import os
 import glob
+import pandas as pd
+import numpy as np
 
 import sys
 sys.path.append('/home/arthur/ISEN/Recherche/Stegano/work/db/FMA/fma/arthur_modifs/')
 import utilsFMA
 # only after this import our "utils" module!
 
-from utils_audio import mp3_to_wav
+from utils_audio import mp3_to_wav, selectSounds_FMA
+
+def main():
+    ###################################
+    # FLAGS
+    sound_selection = 1 # use to browse the data base and collect the sounds we want (may be run only once)
+    convert_format = 0 # use to convert selected sounds from mp3 to wav, if needed
+    encode = 0 # use to encode the files from a given directory
+    verbose = 0 # verbose mode for some functions
+
+    ###################################
+    # PATHS
+    dir_downloaded_db = '/media/arthur/DD_Sonif/db/fma_small/' # where all sounds have been stored (raw db)
+    output_db_dir = '/home/arthur/ISEN/Recherche/Stegano/work/db/FMA/working-dir/'
+    dir_metadata = '/media/arthur/DD_Sonif/db/fma_metadata/'
+    metadata = 'tracks.csv'
+    ###################################
+    # GENERAL PARAMETERS
+    display = 0
+
+    ###################################
+    # OTHER PARAMETERS
+    # --- these ones for browsing the db and selecting files
+    my_genre = 'Metal'
+    min_dur = 10# in seconds
+    max_dur = 11# in seconds
+    dest_dir_selected_sounds = '/home/arthur/ISEN/Recherche/Stegano/work/db/FMA/working-dir/'
+    # --- these ones for loading and encoding
+    encode_source_dir = '/home/arthur/ISEN/Recherche/Stegano/work/db/FMA/working-dir/'
+    encode_dest_dir = '/home/arthur/ISEN/Recherche/Stegano/work/db/FMA/working-dir/encoded/' # NOT IN USE
+    wmkFile_dir = '/home/arthur/ISEN/Recherche/Stegano/work/'
+    wmkFile = 'watermark_test'
+    keyFile = 'key_test'
+    encoding_alg = 'LSB' # 'DSS' 'LSB'
+
+    if sound_selection:
+        print('---- scanning database ----')
+        list_keys_selectedFiles,track_ids = selectSounds_FMA(dir_metadata,metadata,dir_metadata,'genres.csv',genre=my_genre,min_dur=min_dur,max_dur=max_dur,verbose=verbose)
+        print('---- copying sounds ----')
+        copySounds_FMA(list_keys_selectedFiles,track_ids,dir_downloaded_db,dest_dir_selected_sounds)
+       
+    if convert_format: 
+        print('---- mp3->wav conversion ----')
+        mp3_to_wav(dest_dir_selected_sounds, rm_mp3=1) #/!\ will remove the mp3 files once converted to wav
+
+    if encode:
+        pass
+
+    print('DONE... NOW POSSIBLY RUN <sudo rm *.mp3> IN YOUR WORKING DIRECTORY')
 
 ###################################
-dir_downloaded_db = '/media/arthur/DD_Sonif/db/fma_small/' # where all sounds have been stored (raw db)
-output_db_dir = '/home/arthur/ISEN/Recherche/Stegano/work/db/FMA/working-dir/'
-dir_metadata = '/media/arthur/DD_Sonif/db/fma_metadata/'
-#file_metadata = 'raw_tracks.csv' #for 'raw_tracks.csv', interesting fields are track_url, track_genres, track_duration, format 'MM:SS'
- #other files for metadata: genres.csv, raw_albums.csv, raw_artists.csv, raw_genres.csv, raw_tracks.csv, tracks.csv
-#tracks.csv: fields: track/genres, 
+if __name__ == "__main__":
+    main()
 
-#tracks = utilsFMA.load(dir_metadata + 'raw_tracks.csv')
-#genres = utilsFMA.load(dir_metadata + 'genres.csv')
 
-#print(genres)
-
-f = utilsFMA.get_audio_path(dir_downloaded_db, 2000) # /!\ --> change 2 with the key that is wanted
-filename = f.split('/')[-1]
-path = '/'.join(f.split('/')[:-1]) + '/'
-
-if os.path.exists(path+filename):
-    print('Found file ' + str(path+filename) + '! -> retrieving it...')
-    os.system('cp ' + path+filename + ' ' + output_db_dir + filename)
-else:
-    print('File ' + str(current_path) + ' not found... trying next one')
-
-########## CONVERT TO WAV
-mp3_to_wav(output_db_dir+filename,rm_mp3=0)
-
-#x, sr = librosa.load(filename[:-4] + '.wav', sr=None, mono=True) # mono=True???
-signal = audioread.audio_open(output_db_dir+filename[:-4] + '.wav')
-
-print('DONE... NOW POSSIBLY RUN <sudo rm *.mp3> IN YOUR WORKING DIRECTORY')
